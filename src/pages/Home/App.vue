e93c24bbb0e51379a13b147abf76ede8c121b486
rusakov <dan-rusakov@mail.ru>
2020-09-13 01:00:12 +0500

    add welcome-section

--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
     <Header />
+    <section class="welcome-section">
+      <div class="welcome-section__wrapper">
+        <h1 class="welcome-section__title">Гимнастами не рождаются – ими становятся</h1>
+        <ul class="welcome-section__contacts">
+          <li class="welcome-section__contacts-item">—  ул. Белинского 97</li>
+          <li class="welcome-section__contacts-item">—  +7 (922) 357-84-61</li>
+        </ul>
+        <img src="../../assets/img/ribbon.png" alt="Лента" class="welcome-section__ribbon">
+      </div>
+    </section>
   </div>
 </template>
 
