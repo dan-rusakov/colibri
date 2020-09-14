c8da5c9587f69a2962a4352182e862789d6a9999
rusakov <dan-rusakov@mail.ru>
2020-09-15 00:02:38 +0500

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrapper">
+      <p class="footer__copyright">© 2020</p>
+      <ul class="footer__soc-box">
+        <li class="footer__soc-box-item">
+          <a href="#!" class="footer__soc-box-url">
+            <svg width="26" height="26" class="footer__soc-box-icon">
+              <use xlink:href="#icon-vk" />
+            </svg>
+          </a>
+        </li>
+        <li class="footer__soc-box-item">
+          <a href="#!" class="footer__soc-box-url">
+            <svg width="26" height="26" class="footer__soc-box-icon">
+              <use xlink:href="#icon-whatsapp" />
+            </svg>
+          </a>
+        </li>
+        <li class="footer__soc-box-item">
+          <a href="#!" class="footer__soc-box-url">
+            <svg width="26" height="26" class="footer__soc-box-icon">
+              <use xlink:href="#icon-instagram" />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'Footer',
+
+  }
+</script>
