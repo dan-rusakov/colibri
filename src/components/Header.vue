1a359bc93e05f96f052d8274b2b6aa1f701fb71e
rusakov <dan-rusakov@mail.ru>
2020-10-16 00:00:58 +0500

    add main page adaptive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,20 +4,43 @@
       <a href="/" class="header__logo-url">
         <img src="../assets/img/logo.svg" alt="Колибри" class="header__logo">
       </a>
-      <nav class="header_navigation">
+      <nav class="header__navigation">
         <ul class="header__menu">
-          <li class="header_menu-item">
+          <li class="header__menu-item">
             <a href="#!" class="header__menu-url">О нас</a>
           </li>
-          <li class="header_menu-item">
+          <li class="header__menu-item">
             <a href="#!" class="header__menu-url">Услуги</a>
           </li>
-          <li class="header_menu-item">
+          <li class="header__menu-item">
             <a href="#!" class="header__menu-url">Расписание и цены</a>
           </li>
         </ul>
         <a href="#!" class="header__contact-btn">Связаться</a>
       </nav>
+      <button
+          type="button"
+          :class="['header__burger', {'header__burger--active': mobileMenuOpened}]"
+          @click="mobileMenuOpened = !mobileMenuOpened"
+      >
+        <span class="header__burger-line"></span>
+      </button>
+      <transition name="slide-down">
+        <div class="header__mobile-menu-wrap" v-if="mobileMenuOpened">
+          <ul class="header__mobile-menu">
+            <li class="header__mobile-menu-item">
+              <a href="#!" class="header__mobile-menu-url">О нас</a>
+            </li>
+            <li class="header__mobile-menu-item">
+              <a href="#!" class="header__mobile-menu-url">Услуги</a>
+            </li>
+            <li class="header__mobile-menu-item">
+              <a href="#!" class="header__mobile-menu-url">Расписание и цены</a>
+            </li>
+          </ul>
+          <a href="#!" class="header__contact-btn header__contact-btn--mobile">Связаться</a>
+        </div>
+      </transition>
     </div>
   </header>
 </template>
@@ -25,6 +48,10 @@
 <script>
   export default {
     name: 'Header',
-
+    data() {
+      return {
+        mobileMenuOpened: false,
+      }
+    }
   }
 </script>
--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="main">
     <Header />
     <section class="welcome-section">
       <div class="welcome-section__wrapper">
